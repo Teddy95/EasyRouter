dde13dcc6ae9ae68bfe3e8ada8769f405c39547a
Teddy95 <webmaster@andre-sieverding.de>
2014-02-01 04:55:20 +0100

    Update route.php

--- src/route.php
+++ src/route.php
@@ -28,6 +28,9 @@
 
 namespace Teddy95\EasyRouter;
 
+/**
+ * Routing class
+ */
 class route
 {
 
@@ -42,7 +45,20 @@ class route
 	public static $src = "https://github.com/Teddy95/EasyRouter";
 
 	/**
-	 * @return	returns an array with the uri-params on success or FALSE on failure
+	 * Parameters for functions.
+	 */
+	private static $paramsCount = 0;
+	private static $prepareCount = 0;
+	private static $routed = false;
+	private static $prepareString = false;
+
+	/**
+	 * @param string	$basedir
+	 * @param array		$params
+	 * @param array		$exceptions
+	 * @param bool		$load_GET
+	 *
+	 * @return array	Returns an array with the uri-params on success or FALSE on failure
 	 */
 	public static function start ($basedir = null, $params = null, $exceptions = null, $load_GET = true)
 	{
@@ -104,6 +120,8 @@ class route
 		$uri = str_replace($basedir . "/", "", $uri . $slash);
 
 		if (is_null($params) === TRUE) {
+
+			$paramsCount = 0;
 			
 			/**
 			 * Load the individual parameters in the $_GET array.
@@ -144,6 +162,8 @@ class route
 			}
 
 		} else {
+
+			$paramsCount = count($params);
 			
 			/**
 			 * Load the individual parameters in the $route array.
@@ -260,6 +280,7 @@ class route
 								goto paramEnd;
 							} else {
 								$k++;
+								$paramsCount--;
 							}
 						}
 					}
@@ -291,6 +312,178 @@ class route
 			}
 		}
 
+		self::$paramsCount = $paramsCount;
+		self::$routed = true;
+
+		if ($load_GET === TRUE) {
+			$GLOBALS['_GET'] = $__GET;
+			
+			return;
+		} else {
+			/**
+			 * Return array.
+			 */
+			return $__GET;
+		}
+		
+	}
+
+	/**
+	 * @param string	$basedir
+	 * @param bool		$load_GET
+	 *
+	 * @return array	Returns an array with the uri-params on success or FALSE on failure
+	 */
+	public static function execute ($basedir = null, $load_GET = true)
+	{
+
+		if (self::$prepareString == FALSE) {
+			return false;
+		}
+
+		/**
+		 * Current path.
+		 */
+		if (is_null($basedir)) {
+			if (!isset($_SERVER['HTTPS'])) {
+				$uri = "http://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+			} else {
+				$uri = "https://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+			}
+		} else {
+			if (substr($basedir, 0, 7) == "http://") {
+				$uri = "http://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+			} elseif (substr($basedir, 0, 8) == "https://") {
+				$uri = "https://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+			} else {
+				return false;
+			}
+		}
+		
+		/**
+		 * Generate basedir.
+		 */
+		if (is_null($basedir)) {
+			if (!isset($_SERVER['HTTPS'])) {
+				$scheme = 'http://';
+			} else {
+				$scheme = 'https://';
+			}
+			$basedir = $scheme . $_SERVER['SERVER_NAME'] . $_SERVER['SCRIPT_NAME'];
+			$basedir = str_replace('/' . basename($scheme . $_SERVER['SERVER_NAME'] . $_SERVER['SCRIPT_NAME']), '', $basedir);
+		}
+		
+		/**
+		 * Delete last slash from the root directory.
+		 */
+		if (substr($basedir, -1, 1) == "/") {
+			$basedir = substr($basedir, 0, strlen($basedir) - 1);
+		}
+		
+		/**
+		 * Is there a slash at the end of the path? (only if there are no parameters!)
+		 */
+		$slash = "";
+		if ($uri == $basedir) {
+			$slash = "/";
+		} elseif ($uri == $basedir . "/") {
+			$slash = "";
+		} elseif (substr($uri, -1, 1) == "/") {
+			$uri = substr($uri, 0, strlen($uri) - 1);
+		}
+		
+		/**
+		 * Filter out the index file and the base directory.
+		 */
+		$uri = str_replace($basedir . "/", "", $uri . $slash);
+
+		$paramsCount = 0;
+			
+		/**
+		 * Parse prepare string and load the individual parameters in the $_GET array.
+		 */
+		$prepareString = self::$prepareString;
+		$parts = explode('/', $prepareString);
+		$partsCount = count($parts);
+		$prepareParams = array();
+		$uriParams = array();
+		$uriParts = explode('/', $uri);
+
+		for ($i = 0; $partsCount > $i; $i++) {
+			if (strpos($parts[$i], '-') == TRUE) {
+				$prepareParams = explode('-', $parts[$i]);
+				$uriParams = explode('-', $uriParts[$i], count($prepareParams));
+				
+				for ($j = 0; count($prepareParams) > $j; $j++) {
+					if (isset($uriParams[$j]) && !is_null($uriParams[$j]) && !empty($uriParams[$j])) {
+						$__GET[substr($prepareParams[$j], 1, strlen($prepareParams[$j]) - 2)] = $uriParams[$j];
+						$latestKey = substr($prepareParams[$j], 1, strlen($prepareParams[$j]) - 2);
+						if ($j > 0) {
+							self::$prepareCount++;
+						}
+					}
+				}
+
+				$prepareParams = $parts[$i];
+				$uriParams = $uriParts[$i];
+
+				if (isset($uriParams) && !is_null($uriParams) && !empty($uriParams)) {
+					$paramsCount++;
+				}
+			} else {
+				$prepareParams = $parts[$i];
+				$uriParams = $uriParts[$i];
+
+				if (isset($uriParams) && !is_null($uriParams) && !empty($uriParams)) {
+					$__GET[substr($prepareParams, 1, strlen($prepareParams) - 2)] = $uriParams;
+					$latestKey = substr($prepareParams, 1, strlen($prepareParams) - 2);
+					$paramsCount++;
+				}
+			}
+		}
+
+		if (count($uriParts) - 1 >= $i) {
+			$remains = count($uriParts) - 1 - $i;
+			for ($j = 0; $remains >= $j; $j++) {
+				$__GET[$j + $i + 1] = $uriParts[$j + $i];
+			}
+		}
+
+		if (strpos($__GET[$latestKey], '?') == true) { # a question mark must be present in the parameter
+			if (substr($__GET[$latestKey], -1, 1) == '?') { # is the last character a question mark?
+				$__GET[$latestKey] = substr($__GET[$latestKey], 0, strlen($__GET[$latestKey]) - 1);
+				$glue = "?";
+				$glueGetParam = false;
+			} else {
+				$glue = "";
+				$glueGetParam = false;
+			}
+			if (strpos($__GET[$latestKey], '?') == true) {
+				$glueGetParam = true;
+				$elements = explode('?', $__GET[$latestKey], 2);
+				$__GET[$latestKey] = $elements[0];
+				$elements[1] .= $glue;
+				$getParams = explode('&', $elements[1]);
+				$trueGetParams = array();
+				foreach ($getParams as $getParam) {
+					if (strpos($getParam, '=') == true) {
+						$paramArray = explode('=', $getParam, 2);
+						$trueGetParams[$paramArray[0]] = $paramArray[1];
+					}
+				}
+			}
+			$__GET[$latestKey] .= $glueGetParam == false ? $glue : '';
+		}
+
+		if (isset($trueGetParams) && count($trueGetParams) > 0) {
+			foreach ($trueGetParams as $key => $value) {
+				$__GET[$key] = $value;
+			}
+		}
+
+		self::$paramsCount = $paramsCount;
+		self::$routed = true;
+
 		if ($load_GET === TRUE) {
 			$GLOBALS['_GET'] = $__GET;
 			
@@ -305,7 +498,97 @@ class route
 	}
 	
 	/**
-	 * @return	returns an array with information about EasyRouter
+	 * @param string	$prepareString
+	 */
+	public static function prepare ($prepareString)
+	{
+
+		if (substr($prepareString, -1, 1) == '/') {
+			$prepareString = substr($prepareString, 0, strlen($prepareString)-1);
+		}
+
+		if (substr($prepareString, 0, 1) == '/') {
+			$prepareString = substr($prepareString, 1, strlen($prepareString)-1);
+		}
+
+		$prepareString = str_replace('}-{', '}#{', $prepareString);
+		$prepareString = str_replace(array(' ', '-'), array('_', '_'), $prepareString);
+		$prepareString = str_replace('}#{', '}-{', $prepareString);
+		self::$prepareString = $prepareString;
+
+	}
+	
+	/**
+	 * @return array	Returns an array with information about EasyRouter
+	 */
+	public static function get_true_params ()
+	{
+
+		$trueGetParams = array();
+
+		if (isset($_SERVER['QUERY_STRING'])) {
+			if (!empty($_SERVER['QUERY_STRING'])) {
+				$params = explode('&', $_SERVER['QUERY_STRING']);
+				foreach ($params as $param) {
+					$paramArray = explode('=', $param, 2);
+					$trueGetParams[$paramArray[0]] = $paramArray[1];
+				}
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+
+		return $trueGetParams;
+
+	}
+	
+	/**
+	 * @param string	$href
+	 * @param int		$params
+	 *
+	 * @return bool		Returns false on failure
+	 */
+	public static function error ($href, $params = null)
+	{
+
+		if (isset(self::$routed) && self::$routed == TRUE) {
+			if (!isset($params) || is_null($params)) {
+				if (!self::get_true_params()) {
+					$trueGetParams = 0;
+				} else {
+					$trueGetParams = count(self::get_true_params());
+				}
+
+				$paramsCount = self::$paramsCount + $trueGetParams;
+
+				if (count($_GET) - self::$prepareCount > $paramsCount) {
+					header('Location: ' . $href);
+					exit();
+				}
+			} else {
+				if (!self::get_true_params()) {
+					$trueGetParams = 0;
+				} else {
+					$trueGetParams = count(self::get_true_params());
+				}
+
+				$paramsCount = $params + $trueGetParams;
+
+				if (count($_GET) - self::$prepareCount > $paramsCount) {
+					header('Location: ' . $href);
+					exit();
+				}
+			}
+		} else {
+			return false;
+		}
+
+	}
+	
+	/**
+	 * @return array	Returns an array with information about EasyRouter
 	 */
 	public static function info ()
 	{
