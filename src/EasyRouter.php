7be33bd71b0f0238ca8e533cecfea030389f3afc
Andre Sieverding <webmaster@andre-sieverding.de>
2014-04-10 00:44:24 +0200

    Update EasyRouter.php

--- src/EasyRouter.php
+++ src/EasyRouter.php
@@ -5,7 +5,7 @@
  * 
  * The MIT License (MIT)
  * 
- * Copyright (c) 2014 EasyRouter
+ * Copyright (c) 2014 Andre Sieverding
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
